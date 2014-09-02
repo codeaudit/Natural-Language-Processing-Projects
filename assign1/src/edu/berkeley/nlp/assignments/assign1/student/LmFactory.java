9517143707e8c91e2cd3bec83ecf6799cacb9ede
Anting Shen <shenantingtt@hotmail.com>
2014-09-01 22:51:49 -0700

    initial stuff

---
+++ assign1/src/edu/berkeley/nlp/assignments/assign1/student/LmFactory.java
@@ -0,0 +1,17 @@
+package edu.berkeley.nlp.assignments.assign1.student;
+
+import java.util.List;
+
+import edu.berkeley.nlp.langmodel.LanguageModelFactory;
+import edu.berkeley.nlp.langmodel.NgramLanguageModel;
+
+public class LmFactory implements LanguageModelFactory
+{
+
+	public NgramLanguageModel newLanguageModel(Iterable<List<String>> trainingData) {
+
+		 return null; // TODO Construct an exact LM implementation here.
+
+	}
+
+}
