b33bceb2bc85a2f70abcf5537abfca7517720f87
Anting Shen <shenantingtt@hotmail.com>
2014-11-11 16:28:17 -0800

    back to maxent

--- assign4/src/edu/berkeley/nlp/assignments/rerank/student/AwesomeParsingRerankerFactory.java
+++ assign4/src/edu/berkeley/nlp/assignments/rerank/student/AwesomeParsingRerankerFactory.java
@@ -20,7 +20,7 @@ import java.util.*;
 public class AwesomeParsingRerankerFactory implements ParsingRerankerFactory {
 
   public ParsingReranker trainParserReranker(Iterable<Pair<KbestList,Tree<String>>> kbestListsAndGoldTrees) {
-     return new PerceptronReranker(kbestListsAndGoldTrees);
+     return new MaximumEntropyReranker(kbestListsAndGoldTrees);
   }
 }
 
