fecf1e2f755b5a767d65876728f44bebfa06d6f2
Anting Shen <shenantingtt@hotmail.com>
2014-11-15 18:10:52 -0800

    heuristic test

--- assign5/src/edu/berkeley/nlp/assignments/align/student/HeuristicAlignerFactory.java
+++ assign5/src/edu/berkeley/nlp/assignments/align/student/HeuristicAlignerFactory.java
@@ -9,6 +9,7 @@ import edu.berkeley.nlp.mt.decoder.Decoder;
 import edu.berkeley.nlp.mt.decoder.DecoderFactory;
 import edu.berkeley.nlp.mt.decoder.DistortionModel;
 import edu.berkeley.nlp.mt.phrasetable.PhraseTable;
+import edu.berkeley.nlp.util.Counter;
 
 public class HeuristicAlignerFactory implements WordAlignerFactory
 {
@@ -22,12 +23,45 @@ public class HeuristicAlignerFactory implements WordAlignerFactory
 
 class HeuristicAligner implements WordAligner {
 
+	Counter<String> frenchCounter = new Counter<String>();
+	Counter<String> englishCounter = new Counter<String>();
+	Counter<String> pairCounter = new Counter<String>();
+
 	HeuristicAligner(Iterable<SentencePair> trainingData) {
-		System.out.println("hi");
+		for (SentencePair sentencePair : trainingData) {
+			for (String englishWord : sentencePair.getEnglishWords()) {
+				englishCounter.incrementCount(englishWord, 1);
+				for (String frenchWord : sentencePair.getFrenchWords()) {
+					pairCounter.incrementCount(englishWord + "^" + frenchWord, 1);
+				}
+			}
+			for (String frenchWord : sentencePair.getFrenchWords()) {
+				frenchCounter.incrementCount(frenchWord, 1);
+			}
+		}
 	}
 
 	public Alignment alignSentencePair(SentencePair sentencePair) {
 
-		return null;
+		Alignment alignment = new Alignment();
+
+		int frenchPos = 0;
+		for (String frenchWord : sentencePair.getFrenchWords()) {
+			int englishPos = 0;
+			int bestPos = 0;
+			double bestScore = Double.NEGATIVE_INFINITY;
+			for (String englishWord : sentencePair.getEnglishWords()) {
+				double score = pairCounter.getCount(englishWord + "^" + frenchWord);
+				score /= (englishCounter.getCount(englishWord) * englishCounter.getCount(frenchWord));
+				if (score > bestScore) {
+					bestPos = englishPos;
+					bestScore = score;
+				}
+				englishPos++;
+			}
+			alignment.addAlignment(bestPos, frenchPos, true);
+			frenchPos++;
+		}
+		return alignment;
 	}
 }
